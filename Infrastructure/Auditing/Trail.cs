d179b51415181ae7aeed8783abe9cf4ceab29e52
Gabriel Ichim <ichimgabriel75@gmail.com>
2022-06-15 10:05:23 +0100

    Infrastr/auditing-jwt

---
+++ Infrastructure/Auditing/Trail.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Common.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Auditing
+{
+    public class Trail : BaseEntity
+    {
+        public Guid UserId { get; set; }
+        public string? Type { get; set; }
+        public string? TableName { get; set; }
+        public DateTime DateTime { get; set; }
+        public string? OldValues { get; set; }
+        public string? NewValues { get; set; }
+        public string? AffectedColumns { get; set; }
+        public string? PrimaryKey { get; set; }
+    }
+}
---
+++ Infrastructure/Auth/Jwt/JwtSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Auth.Jwt
+{
+    public class JwtSettings
+    {
+        public string? Key { get; set; }
+
+        public int TokenExpirationInMinutes { get; set; }
+
+        public int RefreshTokenExpirationInDays { get; set; }
+    }
+}
